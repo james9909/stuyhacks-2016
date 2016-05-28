8e59e627290221670fff1646f2b6fe18baa40bc8
James Wang <jameswang9909@hotmail.com>
2016-05-28 12:42:14 -0400

    Add basic SQLAlchemy stuff

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    uid = db.Column(db.Integer, unique=True, primary_key=True)
+    email = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(128))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+with app.app_context():
+    from api.models import db, Users
+    db.init_app(app)
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
